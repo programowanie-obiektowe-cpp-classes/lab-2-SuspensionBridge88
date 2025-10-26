00f9f0c438f167db2e5312c0b360518f7bf435f7
SuspensionBridge88 <74118806+SuspensionBridge88@users.noreply.github.com>
2025-10-26 22:38:46 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,43 @@
 #pragma once
 
 #include "Resource.hpp"
+#include <iostream>
 
 class ResourceManager
 {
     // Twoja implementacja tutaj
+public:
+    ResourceManager();
+    {
+        rsrc = NULL;
+        liczba = NULL;
+    }
+    ~ResourceManager()
+    {
+        std::cout << : "Usunieto kopie zasobu";
+    }
+    double get()
+    {
+        liczba = rsrc.get();
+        return liczba;
+    }
+    ResourceManager(const ResourceManager& kopia)
+    {
+        liczba = kopia.liczba;
+        rsrc = kopia.rsrc;
+    }
+    ResourceManager operator=(const ResourceManager& wartosc)
+    {
+        liczba = wartosc.liczba;
+        rsrc = wartosc.rsrc;
+    }
+    ResourceManager(ResourceManager&& zmiana) noexcept: liczba{zmiana.liczba}, rsrc{zmiana.rsrc}
+    {
+        zmiana.liczba = NULL;
+        zmiana.rsrc = NULL;
+    }
+        
+private:
+    double liczba;
+    Resource rsrc;
 };
